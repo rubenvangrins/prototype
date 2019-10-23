525d9a073eee7ec3e156c37bcc25e511bdd0b1c9
Ruben v. G <Rubenvangrinsven@hotmail.com>
2019-10-23 15:46:34 +0200

    reorganised three init & added stats.js

--- index.js
+++ index.js
@@ -1,36 +1,48 @@
 import './style.scss'
-import * as THREE from 'three';
-import * as OrbitControls from 'three-orbitcontrols';
+import * as THREE from 'three'
+import * as OrbitControls from 'three-orbitcontrols'
+import Stats from 'stats.js'
 
-let scene = new THREE.Scene();
-let camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(2, 1, 0);
+let scene, camera, renderer, controls, stats
 
-let renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+const init = () => {
+    scene = new THREE.Scene();
 
-let controls = new THREE.OrbitControls(camera, renderer.domElement);
+    camera = new THREE.PerspectiveCamera(50, innerWidth / innerHeight, 0.1, 1000);
+    camera.position.set(0, 0, 10);
 
-let dome = new THREE.Mesh(
-    new THREE.SphereGeometry(50, 32, 32).scale(-1, 1, 1),
-    new THREE.MeshBasicMaterial({
-         wireframe: true
+    renderer = new THREE.WebGLRenderer( {
+        antialias: true
+    });
+
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(renderer.domElement)
+
+    controls = new THREE.OrbitControls(camera, renderer.domElement)
+
+    stats = new Stats();
+    stats.showPanel( 0 );
+    document.body.appendChild( stats.dom );
+
+    window.addEventListener('resize', () => {
+        camera.aspect = window.innerWidth / window.innerHeight
+        camera.updateProjectionMatrix()
+
+        renderer.setSize( window.innerWidth, window.innerHeight ); 
     })
-);
-
-scene.add(dome);
-
-window.addEventListener('resize', () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize( window.innerWidth, window.innerHeight );  
-})
+}
 
 const animate = () => {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera)
-};
+    stats.begin();
 
-new animate();
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+    controls.update();
+
+	stats.end();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    new init()
+    new animate()
+});
